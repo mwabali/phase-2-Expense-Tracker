c1db5581ca647d478032f14672e79c06bd230af7
mwabali <martinmwash18952058@gmail.com>
2025-11-25 00:48:32 +0300

    "Added the expenseFilter logic"

---
+++ src/components/ExpenseFilter.jsx
@@ -0,0 +1,12 @@
+function ExpenseFilter({ filter, setFilter }) {
+  return (
+    <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+      <option>All</option>
+      <option>Food</option>
+      <option>Transport</option>
+      <option>Entertainment</option>
+      <option>Other</option>
+    </select>
+  );
+}
+export default ExpenseFilter;
