a92b7e6938d3a8ec0880a56970b4a9b4a26da59b
mwabali <martinmwash18952058@gmail.com>
2025-11-25 00:30:26 +0300

    "Added usecases"

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,13 @@ import { useState } from 'react';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 import ExpenseFilter from './components/ExpenseFilter';
+
+function App(){
+   const [expenses, setExpenses] = useState([]);
+  const [filter, setFilter] = useState('All');
+  const [editingExpense, setEditingExpense] = useState(null);
+
+  return(
+
+  )
+}
